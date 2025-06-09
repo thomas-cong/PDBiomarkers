fb48763190ed2c7974bdee897ca2cfea59f6236f
Thomas Cong <crazycolors27@gmail.com>
2025-06-09 08:53:07 -0400

    updated scatterplot for feature comparison to also fit a regression line

--- data_visualization/make_scatterplot.py
+++ data_visualization/make_scatterplot.py
@@ -18,15 +18,15 @@ def feature_comparison_scatterplot(df1, df2, feature):
     df2_feature_vector = intersected_df2[feature]
     
     f, ax = plt.subplots(figsize=(6, 6), dpi=300)
-    ax.set_xlabel('True')
-    ax.set_ylabel('Test')
     # Create linspace instead of arange to ensure proper number of points for the y=x line
     min_val = min(df1_feature_vector.min(), df2_feature_vector.min())
     max_val = max(df1_feature_vector.max(), df2_feature_vector.max())
     line_x = np.linspace(min_val, max_val, 100)  # 100 points should be sufficient
     sns.lineplot(x=line_x, y=line_x, ax=ax, color='red')
     ax.lines[0].set_linestyle("--")
-    sns.scatterplot(x=df1_feature_vector, y=df2_feature_vector, ax=ax, alpha=0.5)
+    sns.regplot(x=df1_feature_vector, y=df2_feature_vector, ax=ax, scatter_kws={'alpha': 0.5})
+    ax.set_xlabel('True')
+    ax.set_ylabel('Test')
     f.suptitle(feature)
     return f
 
