6687afc492937ec26845cba63f06f8f14efa73be
Thomas Cong <crazycolors27@gmail.com>
2025-06-11 11:36:22 -0400

    refactor: convert heatmap script into reusable function for batch processing clinical biomarker data

--- R Visualizations/heatmap.R
+++ R Visualizations/heatmap.R
@@ -5,24 +5,33 @@ library(reshape2)
 library(dplyr)
 library(tidyr)
 library(ggthemes)
-heatmap_data <- read.csv("2157-Generated-Data/2157-Clinical-Biomarkers-Normalized.csv")
-heatmap_long_data <- heatmap_data %>%
-    select(-any_of("subjid")) %>% 
-    pivot_longer(cols = -c(winterlight_id, Status),      
-                 names_to = "biomarker", 
-                 values_to = "value") %>%     
-    mutate(
-        winterlight_id = as.factor(winterlight_id),
-        Status = as.factor(Status) 
-    )
-hm <-ggplot(heatmap_long_data, aes(x = winterlight_id, y = biomarker, fill = value)) +
-  geom_tile() +
-  scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
-  labs(title = "Heatmap of Biomarker Values by Status") + 
-  facet_grid(~ Status, scales = "free_x", space = "free_x") + 
-  theme_tufte(base_family="Helvetica") + theme(
-    axis.title.x=element_blank(),
-    axis.text.x=element_blank(),
-    axis.ticks.x=element_blank()
-    )
-ggsave("heatmap.png", plot = hm, width = 24, height = 8, dpi = 300)
+heatmap_function <- function(file_path, save_path, title){
+    heatmap_data <- read.csv(file_path)
+    heatmap_long_data <- heatmap_data %>%
+        select(-any_of("subjid")) %>% 
+        pivot_longer(cols = -c(winterlight_id, Status),      
+                    names_to = "biomarker", 
+                    values_to = "value") %>%     
+        mutate(
+            winterlight_id = as.factor(winterlight_id),
+            Status = as.factor(Status) 
+        )
+    hm <-ggplot(heatmap_long_data, aes(x = winterlight_id, y = biomarker, fill = value)) +
+    geom_tile() +
+    scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0) +
+    labs(title = title) + 
+    facet_grid(~ Status, scales = "free_x", space = "free_x") + 
+    theme_tufte(base_family="Helvetica") + theme(
+        axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank()
+        )
+    ggsave(save_path, plot = hm, width = 24, height = 8, dpi = 300)
+}
+files <- list.files("2157-Generated-Data/Clinical", pattern = "normalized.csv", full.names = TRUE)
+for (file in files){
+    file_name <- tail(strsplit(file, "/")[[1]], 1)    
+    save_path <- paste0("./R Figures/", gsub("normalized.csv", "heatmap.png", file_name))
+    heatmap_function(file, save_path, paste(gsub("normalized.csv", "", file_name), "Biomarker Heatmap"))
+}
+
